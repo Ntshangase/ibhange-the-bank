09f831b718a6bdc51b9abe67e0fea262076b87a0
Ntshangase <simphiwe323i@gmail.com>
2023-04-19 19:48:04 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   const title = "Welcome to the new blog";
 
   return (
     <div className="App">
+      <Navbar />
       <div className='content'>
         <h1>{ title }</h1>
       </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="navbar">
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/home">Homepage</a>
+        <a href="/login">Login</a>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;

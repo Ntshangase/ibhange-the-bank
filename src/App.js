afc5ec1a88dd3adf0632f55726bfa84e4782378f
Ntshangase <simphiwe323i@gmail.com>
2023-04-20 18:04:04 +0200

    feat: react router works

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import Navbar from "./Components/Navbar";
 import Homepage from "./Pages/Homepage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Homepage />
+    // replaced switch with routes since this is v.6
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" />
+            <Route path="/home" element={<Homepage />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 

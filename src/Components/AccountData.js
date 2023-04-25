69ef61ae5518b24fea36ca75e23bc2cdc40e543f
Ntshangase <simphiwe323i@gmail.com>
2023-04-25 23:24:55 +0200

    x

--- src/Components/AccountData.js
+++ src/Components/AccountData.js
@@ -1,26 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const AccountData = ({ zulu }) => { 
+//  Since this is a template, everytime we call it somewhere eles it will render the template as is.
 
-    return (
-        <div>
-            {zulu.map((item) => {
-                return (
-                    <div key={item.id} >
-                        <h2>{item.accountNumber}</h2>
-                        <div>{item.account}</div>
-                        <div>{item.email}</div>
-                        <div>{item.phone}</div>
-                        <div>{item.address}</div>
-                        <div>{item.city}</div>
-                        <div>{item.state}</div>
-                        <div>{item.zip}</div>
-                    </div>
-                )
-            }
-            )}
-        </div>
-    )
-}
+const AccountData = ({ zulu }) => {
+  return (
+    <div>
+      {zulu.map((item) => {
+        return (
+          <div key={item.id} className="accountData">
+            <p>{item.productName}</p>
+            <p> account: {item.accountNumber}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default AccountData
+export default AccountData;

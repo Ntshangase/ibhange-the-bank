f71bf5f263481e728a627037a7114577977f3652
Ntshangase <simphiwe323i@gmail.com>
2023-09-21 03:37:37 +0200

    this thing is hard

--- src/Components/AccountData.js
+++ src/Components/AccountData.js
@@ -4,34 +4,15 @@ import React from "react";
 
 const AccountData = ({ zulu }) => {
   /* postOrder, cardNumber are the two arnormalies in this template,  */
-  function Transact(num) {
-    return (
-      <div>
-        {zulu.map((item) => {
-          return (
-            <div key={item.id}>
-              <p>accountType: {item.transactions[num].type}</p>
-              <p>amount: {item.transactions[num].amount}</p>
-              <p>runningBalance: {item.transactions[num].runningBalance}</p>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
 
   return (
     <div>
       {zulu.map((item) => {
         return (
-          <div>
-            <div key={item.id} className="accountData">
-              <p>Bank Name: {item.bankName}</p>
-              <p> account type: {item.productName}</p>
-              <p> account Number: {item.accountNumber}</p>
-              <p>ID: {item.id}</p>
-              <p>{Transact(2)}</p>
-            </div>
+          <div key={item.id} className="accountData">
+            <p>Bank Name: {item.bankName}</p>
+            <p> account type: {item.productName}</p>
+            <p> account Number: {item.accountNumber}</p>
 
             <button>viewTransactions</button>
           </div>
